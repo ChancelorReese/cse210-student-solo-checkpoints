25f973df0ace5c8d4e0f7218109a210312cca436
ChancelorReese <Chancelor.Reese@gmail.com>
2021-10-20 10:18:01 -0600

    new

--- 06-nim/nim/game/board.py
+++ 06-nim/nim/game/board.py
@@ -1,6 +1,13 @@
 import random
 
 class Board:
+    
+    def __init__():
+        #to_string
+        #apply(move)
+        #is_empty
+    
+    
     def display_board():
         print("---------------------")
         
